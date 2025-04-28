2ad19417f0d4c6d14573ba394d7c415acdd160f5
Nouha <nouhaabednn@icloud.com>
2025-04-29 00:48:20 +0200

    nest

--- src/main/java/fr/ubx/poo/ubgarden/game/go/decor/nest/WaspNest.java
+++ src/main/java/fr/ubx/poo/ubgarden/game/go/decor/nest/WaspNest.java
@@ -1,7 +1,10 @@
 package fr.ubx.poo.ubgarden.game.go.decor.nest;
 
+import fr.ubx.poo.ubgarden.game.Direction;
 import fr.ubx.poo.ubgarden.game.Game;
 import fr.ubx.poo.ubgarden.game.Position;
+import fr.ubx.poo.ubgarden.game.go.enemy.Enemy;
+import fr.ubx.poo.ubgarden.game.go.enemy.Wasp;
 
 public class WaspNest extends Nest {
     public WaspNest(Position position, Game game) {
@@ -17,5 +20,15 @@ public class WaspNest extends Nest {
     public String getEnemyType() {
         return "wasp";
     }
+    @Override
+    protected Enemy createEnemy(Position position) {
+        return new Wasp(game, position, Direction.random());
+    }
+
+    @Override
+    public int getBombCount() {
+        return 1;
+    }
+
 }
 
